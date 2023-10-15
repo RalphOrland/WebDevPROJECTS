517019a509bb00a0f7bab13b7d43335ea48f922e
Ralph <ralphorleynes@gmail.com>
2023-10-15 23:15:22 +0800

    tested some tailwindcss
    just experimenting

---
+++ EndangeredAnimalsMap/animalsoundsGame/tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.js
+module.exports = {
+  // ... other configurations
+  theme: {
+    extend: {
+      colors: {
+        myYellow: {
+          100: '#fff7cc',
+          200: '#ffeb99',
+          300: '#ffdf66',
+          400: '#ffd133',
+          500: '#ffeb3b', // Default color
+          600: '#ffcc00',
+          700: '#e6b800',
+          800: '#cca300',
+          900: '#b38f00',
+        },
+      },
+    },
+  },
+};
